6052a56e77b53f4469575d632812ceea04143f41
Rens Lakens <rens@lakens.org>
2022-04-25 22:47:54 +0200

    edited code layout

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let id = 0;
 
 app.all("*", (req, res, next) => {
     const method = req.method;
+
     console.log(`Method ${method} is aangeroepen`);
     next();
 });
@@ -21,8 +22,10 @@ app.get("/", (req, res) => {
     });
 });
 
+//Make new user
 app.post("/api/user", (req, res) => {
     let user = req.body;
+
     user = {
         id,
         ...user,
@@ -51,7 +54,36 @@ app.post("/api/user", (req, res) => {
     }
 });
 
+//Get user profile
+app.get("/api/user/profile", (req, res) => {
+    res.status(200).json({
+        status: 401,
+        result: `Unauthorized`,
+    });
+});
 
+//Get specific user
+app.get("/api/user/:userId", (req, res, next) => {
+    const userId = req.params.userId;
+
+    console.log(`User met ID ${userId} gezocht`);
+    let user = database.filter((item) => item.id == userId);
+
+    if (user.length > 0) {
+        console.log(user);
+        res.status(201).json({
+            status: 201,
+            result: user,
+        });
+    } else {
+        res.status(403).json({
+            status: 403,
+            result: `User with ID ${userId} not found`,
+        });
+    }
+});
+
+//Edit specific user
 app.put("/api/user/:userId", (req, res) => {
     const userId = req.params.userId;
 
@@ -78,38 +110,15 @@ app.put("/api/user/:userId", (req, res) => {
             result: `User with ID ${userId} not found`,
         });
     }
-
 });
 
-app.get("/api/user/profile", (req, res) => {
-    res.status(200).json({
-        status: 401,
-        result: `Unauthorized`,
-    });
-});
-
-app.get("/api/user/:userId", (req, res, next) => {
+//Delete specific user
+app.delete("/api/user/", (req, res, next) => {
     const userId = req.params.userId;
-    console.log(`User met ID ${userId} gezocht`);
-    let user = database.filter((item) => item.id == userId);
-    if (user.length > 0) {
-        console.log(user);
-        res.status(201).json({
-            status: 201,
-            result: user,
-        });
-    } else {
-        res.status(403).json({
-            status: 403,
-            result: `User with ID ${userId} not found`,
-        });
-    }
-});
 
-app.delete("/api/user/:userId", (req, res, next) => {
-    const userId = req.params.userId;
     console.log(`User with ID ${userId} has been deleted`);
     let user = database.filter((item) => item.id == userId);
+
     if (user.length > 0) {
         console.log(user);
         database.splice(userId, 1);
@@ -125,6 +134,7 @@ app.delete("/api/user/:userId", (req, res, next) => {
     }
 });
 
+//Get all users
 app.get("/api/user", (req, res, next) => {
     res.status(201).json({
         status: 201,

1f6d0b98e93b48022c9f580fb1d35789823a578a
Rens Lakens <rens@lakens.org>
2022-04-29 11:24:58 +0200

    Installed dotenv library

--- dbtest.js
+++ dbtest.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'me',
-    password: 'secret',
+    port: '3306',
+    user: 'root',
+    password: '',
     database: 'share-a-meal'
 });
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
+connection.query('SELECT * from user', function(error, results, fields) {
     if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
+    console.log('The solution is: ', results);
 });
 
 connection.end();

cc047fc0d4b0b0659b605d730a570cd4039a1094
Rens Lakens <rens@lakens.org>
2022-05-11 12:04:44 +0200

    controller aangepast voor testen

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,143 +1,200 @@
 const assert = require('assert');
+const dbconnection = require('../../dbconnection');
 let database = [];
 let id = 0;
 
 let controller = {
     validateUser: (req, res, next) => {
         let user = req.body;
-        let { firstName, lastName, emailAdress, password } = user;
-
+        let {
+            firstName,
+            lastName,
+            emailAdress,
+            password,
+            isActive,
+            phoneNumber,
+            roles,
+            street,
+            city,
+        } = user;
         try {
-            assert(typeof firstName === 'string', 'First Name must be a string');
-            assert(typeof lastName === 'string', 'Last Name must be a string');
-            assert(typeof emailAdress === 'string', 'Email address must be a string');
-            assert(typeof password === 'string', 'Password must be a string');
+            assert(typeof firstName === "string", "firstName must be a string");
+            assert(typeof lastName === "string", "lastName must be a string");
+            assert(typeof emailAdress === "string", "emailAdress must be a string");
+            assert(typeof password === "string", "password must be a string");
+            assert(typeof isActive === "number", "isActive must be either 0 or 1");
+            assert(typeof phoneNumber === "string", "phonenumber must a string");
+            assert(typeof roles === "string", "roles must be a string");
+            assert(typeof street === "string", "steet must be a string");
+            assert(typeof city === "string", "city must be a string");
+            assert(
+                firstName.length > 0,
+                "firstName must be atleast one character long"
+            );
+            assert(
+                lastName.length > 0,
+                "lastName must be atleast one character long"
+            );
+            assert(
+                emailAdress.length > 0,
+                "emailAdress must be atleast one character long"
+            );
+            assert(
+                password.length > 0,
+                "password must be atleast one character long"
+            );
+            assert(isActive == 1 || isActive == 0, "isActive must be a 0 or 1");
+            assert(
+                phoneNumber.length > 0,
+                "phonenumber must be atleast one character long"
+            );
+            assert(
+                roles.includes("editor" || "guest"),
+                "A user must have atleast one role"
+            );
+            assert(street.length > 0, "street must be atleast one character long");
+            assert(city.length > 0, "City must be atleast one character long");
+
+            next();
+        } catch (err) {
+            console.log("User validation found invalid fields!");
+            const error = {
+                status: 400,
+                result: err.message,
+                message: err.message,
+            };
+            next(error);
+        }
+    },
+    validateNumber: (req, res, next) => {
+        try {
+            assert(
+                Number.isInteger(parseInt(req.params.userId)),
+                "Id must be a number"
+            );
             next();
         } catch (err) {
             const error = {
                 status: 400,
-                result: err.message,
+                message: err.message,
             };
-            console.log(error.message);
-            // res.status(400).json({
-            //     status: 400,
-            //     result: error.toString()
-            // });
             next(error);
         }
     },
-
     addUser: (req, res, next) => {
         let user = req.body;
+        const values = [
+            user.firstName,
+            user.lastName,
+            user.isActive,
+            user.emailAdress,
+            user.password,
+            user.phoneNumber,
+            user.roles,
+            user.street,
+            user.city,
+        ];
 
-        user = {
-            id,
-            ...user,
-        };
+        pool.query(
+            "INSERT INTO user (firstName, lastName, isActive, emailAdress, password, phoneNumber, roles, street, city) VALUES (?,?,?,?,?,?,?,?,?)",
+            values,
+            (err, result) => {
+                if (err) {
+                    console.log("User has not been inserted!");
+                    const error = {
+                        status: 409,
+                        result: "User is niet toegevoegd in database",
+                    };
+                    next(error);
+                } else {
+                    console.log(result.insertId);
+                    user.userId = result.insertId;
+                    res.status(201).json({
+                        status: 201,
+                        message: "User is toegevoegd in database",
+                        result: user,
+                    });
+                }
+            }
+        );
+    },
+    getAllUsers: (req, res) => {
+        let users = [];
+        pool.query("SELECT * FROM user", (error, results, fields) => {
+            results.forEach((user) => {
+                users.push(user);
+            });
+            res.status(200).json({
+                status: 200,
+                result: users,
+            });
+        });
+    },
+    getUserById: (req, res, next) => {
+        const userId = req.params.userId;
+        pool.query(
+            `SELECT * FROM user WHERE id =${userId}`,
+            (err, results, fields) => {
+                if (err) throw err;
+                if (results.length > 0) {
+                    res.status(200).json({
+                        status: 200,
+                        result: results,
+                    });
+                } else {
+                    const error = {
+                        status: 404,
+                        message: "User with provided Id does not exist",
+                        result: "User with provided Id does not exist",
+                    };
+                    next(error);
+                }
+            }
+        );
+    },
+    getUserProfile: (req, res) => {
+        res.status(200).json({
+            message: "Not implemented yet",
+        });
+    },
+    updateUser: (req, res, next) => {
+        const userId = req.params.userId;
+        const user = req.body;
+        pool.query(
+            `UPDATE user SET firstName = '${user.firstName}', lastName = '${user.lastName}', street = '${user.street}', city = '${user.city}', emailAdress = '${user.emailAdress}', password = '${user.password}' WHERE id = ${userId}`,
+            (err, results) => {
+                const { changedRows } = results;
+                if (err) throw err;
 
-        const emailCheck = database.some(element => {
-            if (element.email === user.email) {
-                return true;
+                if (changedRows == 0) {
+                    const error = {
+                        status: 404,
+                        message: "User with provided id does not exist",
+                        result: "User with provided id does not exist",
+                    };
+                    next(error);
+                } else {
+                    res.status(200).json({ status: 200, result: "Succusful update!" });
+                }
+            }
+        );
+    },
+    deleteUser: (req, res, next) => {
+        const userId = req.params.userId;
+        pool.query(`DELETE FROM user WHERE id=${userId}`, (err, results) => {
+            if (err) throw err;
+            const { affectedRows } = results;
+            if (!affectedRows) {
+                const error = {
+                    status: 400,
+                    result: "User does not exist",
+                };
+                next(error);
+            } else {
+                res.status(200).json({ status: 200, result: "Succesful deletion" });
             }
         });
-
-        if (emailCheck) {
-            const error = {
-                status: 403,
-                result: `Email already in use`
-            };
-            next(error);
-            console.log(user.email + " already in use");
-            // res.status(403).json({
-            //     status: 403,
-            //     result: ,
-            // });
-        } else {
-            id++;
-            database.push(user);
-            console.log("Added " + user);
-            res.status(201).json({
-                status: 201,
-                result: database,
-            });
-        }
     },
-
-    getAllUsers: (req, res) => {
-        res.status(201).json({
-            status: 201,
-            result: database,
-        });
-    },
-
-    getUserByID: (req, res) => {
-        const userId = req.params.userId;
-
-        console.log(`Looking for user with ID ${userId}`);
-        let user = database.filter((item) => item.id == userId);
-
-        if (user.length > 0) {
-            console.log(user);
-            res.status(201).json({
-                status: 201,
-                result: user,
-            });
-        } else {
-            res.status(403).json({
-                status: 403,
-                result: `User with ID ${userId} not found`,
-            });
-        }
-    },
-
-    editUserByID: (req, res) => {
-        const userId = req.params.userId;
-
-        let oldUser = database.filter((item) => item.id == userId);
-        if (oldUser.length > 0) {
-
-            let newUser = req.body;
-            id = parseInt(userId);
-            newUser = {
-                id,
-                ...newUser,
-            };
-            console.log(newUser);
-            database[userId] = newUser;
-
-            res.status(201).json({
-                status: 201,
-                result: newUser,
-            });
-            console.log(`Modified user ${userId}`);
-        } else {
-            res.status(403).json({
-                status: 403,
-                result: `User with ID ${userId} not found`,
-            });
-        }
-    },
-
-    deleteUserByID: (req, res) => {
-        const userId = req.params.userId;
-
-        console.log(`User with ID ${userId} has been deleted`);
-        let user = database.filter((item) => item.id == userId);
-
-        if (user.length > 0) {
-            console.log(user);
-            database.splice(userId, 1);
-            res.status(201).json({
-                status: 201,
-                result: user,
-            });
-        } else {
-            res.status(403).json({
-                status: 403,
-                result: `User with ID ${userId} not found`,
-            });
-        }
-    }
-}
+};
 
 module.exports = controller;
